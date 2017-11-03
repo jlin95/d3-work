f08dace05b72544f7ff13a4c7dc727151bf9030f
jlin95 <leeway95@gmail.com>
2017-11-03 18:49:37 +0800

    Added padding for space for labels

--- js/heatMap.js
+++ js/heatMap.js
@@ -1,7 +1,13 @@
 d3.json("data/git-commit-frequency.json", function(data) {
-  let width = (20 + 1) * data.length;
-  let height = (20 + 1) * 7;
-  let colors = [ "#1a9850", "#66bd63", "#a6d96a", "#d9ef8b", "#ffffbf", "#fee08b", "#fdae61","#f46d43", "#d73027"];
+  var margin = {
+    top: 20,
+    right: 85,
+    bottom: 30,
+    left: 40
+  }
+  var width = (21 + 1) * data.length;
+  var height = (21 + 1) * 7;
+  var colors = [ "#1a9850", "#66bd63", "#a6d96a", "#d9ef8b", "#ffffbf", "#fee08b", "#fdae61","#f46d43", "#d73027"];
 
   var x = d3.scaleLinear()
     .domain([0, data.length])
